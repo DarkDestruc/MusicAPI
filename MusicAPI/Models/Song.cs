56e2241cf4b9891aa2d54ed05732341a76d8c41c
Moises Chavez <gchavez19956@gmail.com>
2024-01-16 00:44:47 -0500

    Add project files.

---
+++ MusicAPI/Models/Song.cs
@@ -0,0 +1,12 @@
+﻿namespace MusicAPI.Models
+{
+    public class Song
+    {
+        #region Propertis
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Language { get; set; }
+
+        #endregion
+    }
+}
